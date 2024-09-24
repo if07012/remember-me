1821599627fe06d079b6e31e4dd20226438d605b
cpr <cpr@voxteneo.com>
2024-09-24 20:11:43 +0700

    TP #000000 feedback uat
    add is merge class

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,16 +5,14 @@ import Drag from '../ui/dashboard/drag';
 export default function LoginPage() {
     return (
         <main>
-            {/* <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+            <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
                 <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
                     <div className="w-32 text-white md:w-36">
                         <AcmeLogo />
                     </div>
                 </div>
                 <LoginForm />
-            </div> */}
-
-            <Drag/>
+            </div>
         </main>
     );
 }
