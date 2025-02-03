ff862684bb836b0ca41fee0dc86a156cb5bb31c8
cpr <cpr@voxteneo.com>
2025-02-03 11:04:20 +0700

    .

--- app/surat/[name]/page.tsx
+++ app/surat/[name]/page.tsx
@@ -7,7 +7,7 @@ export default async function Page({ params }: { params: { name: string } }) {
     const name = params.name;
     return (
         <main>
-            <Link href={'/surat/albalad'}>Al Balad</Link> -  <Link href={'/surat/allail'}>Al Lail</Link> - <Link href={'/surat/assham'}>AS Shams</Link>  - <Link href={'/surat/alfajr'}>Al Fajr</Link>
+            <Link href={'/surat/albalad'}>Al Balad</Link> -  <Link href={'/surat/allail'}>Al Lail</Link> - <Link href={'/surat/assham'}>AS Shams</Link>  - <Link href={'/surat/alfajr'}>Al Fajr</Link>- <Link href={'/surat/alghashiyah'}>Al-Ghasyiyah </Link>
             <h1 className='text-5xl'>Surat {name}</h1>
             <Drag surat={name}/>
         </main>
